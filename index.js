46767511bc41baada3f4f0050496bcd699b111eb
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2025-10-23 05:47:05 -0300

    feat: detect damage to decrease health points

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ window.addEventListener("keydown", (event) => {
       jogador.atacar()
       break
 
-      case "ArrowRight":
+    case "ArrowRight":
       teclas.ArrowRight.pressed = true
       oponente.ultimaTecla = "ArrowRight"
       break
@@ -215,31 +215,72 @@ function definirVencedor({ jogador, oponente, id_temporizador }) {
 
 // =======================================================
 
-function animate() {
-  window.requestAnimationFrame(animate)
-  c.fillStyle = "black"
-  c.fillRect(0, 0, canvas.width, canvas.height)
-  jogador.atualizar()
-  oponente.atualizar()
+function acertarSoco({ retangulo1, retangulo2 }) {
+  // posicaoAtaqueX1 <= posicaoX2 + largura2(60) && posicaoAtaqueX1 + larguraAtaque1(120) >= posicaoX2
+  // posicaoAtaqueY1 <= posicaoY2 + altura2(300) && posicaoAtaqueY1 + alturaAtaque1(75) >= posicaoY2
+  return (
+    retangulo1.espacoAtaque.posicao.x <=
+      retangulo2.posicao.x + retangulo2.largura &&
+    retangulo1.espacoAtaque.posicao.x + retangulo1.espacoAtaque.largura >=
+      retangulo2.posicao.x &&
+    retangulo1.espacoAtaque.posicao.y <=
+      retangulo2.posicao.y + retangulo2.altura &&
+    retangulo1.espacoAtaque.posicao.y + retangulo1.espacoAtaque.altura >=
+      retangulo2.posicao.y
+  )
+}
 
-  // movimento jogador
-  jogador.velocidade.x = 0
+// =======================================================
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "black";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  jogador.atualizar();
+  oponente.atualizar();
+
+  // movimento jogador 1
+  jogador.velocidade.x = 0;
   if (teclas.a.pressed && jogador.ultimaTecla === "a") {
-    jogador.velocidade.x = -7
+    jogador.velocidade.x = -7;
   } else if (teclas.d.pressed && jogador.ultimaTecla === "d") {
-    jogador.velocidade.x = 7
+    jogador.velocidade.x = 7;
   }
 
-  // movimento oponente
-  oponente.velocidade.x = 0
+  // movimento jogador 2
+  oponente.velocidade.x = 0;
   if (teclas.ArrowLeft.pressed && oponente.ultimaTecla === "ArrowLeft") {
-    oponente.velocidade.x = -7
+    oponente.velocidade.x = -7;
   } else if (
     teclas.ArrowRight.pressed &&
     oponente.ultimaTecla === "ArrowRight"
   ) {
-    oponente.velocidade.x = 7
+    oponente.velocidade.x = 7;
   }
 
+  // detecção de colisão
+  if (
+    acertarSoco({
+      retangulo1: jogador,
+      retangulo2: oponente,
+    }) &&
+    jogador.estaAtacando
+  ) {
+    jogador.estaAtacando = false;
+    oponente.vida -= 20;
+    document.querySelector("#oponenteVida").style.width = oponente.vida + "%";
+  }
+
+  if (
+    acertarSoco({
+      retangulo1: oponente,
+      retangulo2: jogador,
+    }) &&
+    oponente.estaAtacando
+  ) {
+    oponente.estaAtacando = false;
+    jogador.vida -= 20;
+    document.querySelector("#jogadorVida").style.width = jogador.vida + "%";
+  }
 }
 animate()
